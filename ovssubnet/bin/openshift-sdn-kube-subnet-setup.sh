openshift/origin

commit faeb0b86c8075c9266d28d2dd4f4d81b32c80b39

Forward packages to/from cluster_network

ovssubnet/bin/openshift-sdn-kube-subnet-setup.sh
@@ -38,6 +38,9 @@ iptables -D INPUT -i ${TUN} -m comment --comment "traffic from docker for intern
 lineno=$(iptables -nvL INPUT --line-numbers | grep "state RELATED,ESTABLISHED" | awk '{print $1}')
 iptables -I INPUT $lineno -p udp -m multiport --dports 4789 -m comment --comment "001 vxlan incoming" -j ACCEPT
 iptables -I INPUT $((lineno+1)) -i ${TUN} -m comment --comment "traffic from docker for internet" -j ACCEPT
+fwd_lineno=$(iptables -nvL FORWARD --line-numbers | grep "reject-with icmp-host-prohibited" tail -n 1 | awk '{print $1}')
+iptables -I FORWARD $fwd_lineno -d ${cluster_subnet} -j ACCEPT
+iptables -I FORWARD $fwd_lineno -s ${cluster_subnet} -j ACCEPT
 
 ## docker
 if [[ -z "${DOCKER_NETWORK_OPTIONS}" ]]

openshift/openshift-sdn

commit e2a78242588b4dd9e661f8dea70936a468f830c0

kube-subnet-setup: fix setup iptables FORWARD rule
Fixes: faeb0b86c8075c9266d28d2dd4f4d81b32c80b39

ovssubnet/bin/openshift-sdn-kube-subnet-setup.sh
@@ -38,7 +38,7 @@ iptables -D INPUT -i ${TUN} -m comment --comment "traffic from docker for intern
 lineno=$(iptables -nvL INPUT --line-numbers | grep "state RELATED,ESTABLISHED" | awk '{print $1}')
 iptables -I INPUT $lineno -p udp -m multiport --dports 4789 -m comment --comment "001 vxlan incoming" -j ACCEPT
 iptables -I INPUT $((lineno+1)) -i ${TUN} -m comment --comment "traffic from docker for internet" -j ACCEPT
-fwd_lineno=$(iptables -nvL FORWARD --line-numbers | grep "reject-with icmp-host-prohibited" tail -n 1 | awk '{print $1}')
+fwd_lineno=$(iptables -nvL FORWARD --line-numbers | grep "reject-with icmp-host-prohibited" | tail -n 1 | awk '{print $1}')
 iptables -I FORWARD $fwd_lineno -d ${cluster_subnet} -j ACCEPT
 iptables -I FORWARD $fwd_lineno -s ${cluster_subnet} -j ACCEPT
 